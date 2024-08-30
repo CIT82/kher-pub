3138602cf6724e06e65077b8e326669fb66c8d31
Kairi Her <kiwischool0953110@gmail.com>
2024-08-29 19:26:14 -0700

    timer visible

--- w3/script.js
+++ w3/script.js
@@ -5,3 +5,21 @@ let timeElement = document.getElementById('time');
 let score = 0;
 let timeLeft = 25; // 30 seconds timer
 let speed = 987; // initial speed of doggo movement
+
+function moveDog() {
+    let x = Math.floor(Math.random() * (550));
+    let y = Math.floor(Math.random() * (350));
+    dog.style.left = x + 'px';
+    dog.style.top = y + 'px';
+}
+
+function increaseSpeed() {
+    if (speed > 460) {
+        speed -= 35;
+    }
+}
+
+function updateScore() {
+    score++;
+    scoreElement.textContent = score;
+}
