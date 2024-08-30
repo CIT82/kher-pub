3304ec478201ad2a13b40cd446c883e81c6bd201
Kairi Her <kiwischool0953110@gmail.com>
2024-08-29 19:22:52 -0700

    Js test one

---
+++ w3/script.js
@@ -0,0 +1,7 @@
+let dog = document.getElementById('dog');
+let scoreElement = document.getElementById('score');
+let timeElement = document.getElementById('time');
+
+let score = 0;
+let timeLeft = 25; // 30 seconds timer
+let speed = 987; // initial speed of doggo movement
